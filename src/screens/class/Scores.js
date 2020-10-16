7c88e110892afe2ed4484b614245ae02e23fb955
dauntl1ss <35137716+dauntl1ss@users.noreply.github.com>
2020-10-16 23:33:08 +0800

    answer-details-screen completed

--- src/screens/class/Scores.js
+++ src/screens/class/Scores.js
@@ -40,6 +40,7 @@ import {
   KeyboardDatePicker,
 } from "@material-ui/pickers";
 import MomentUtils from "@date-io/moment";
+import { fetchData } from "../../screens/Admin/Dashboard";
 
 function Scores(props) {
   const query = require("query-string").parse(window.location.search);
@@ -634,226 +635,256 @@ function ScoreDetails(props) {
     </React.Fragment>
   );
 }
+
 function ActivityDetails(props) {
   const query = require("query-string").parse(window.location.search);
+  const [errors, setErrors] = useState();
   const [loading, setLoading] = useState(true);
   const { student } = props;
   const theme = useTheme();
   const [no, setNo] = useState(0);
   const [activity, setActivity] = useState([]);
   const [attempt, setAttempt] = useState([]);
-  const [border, setBorder] = useState([]);
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const getActivityDetails = async () => {
     setLoading(true);
-    try {
-      let res = await Api.get(
-        "/api/activity/attempts?activity_id=" +
-          query.activity_id +
-          "&student_id=" +
-          student.id
-      );
-      let res2 = await Api.get(
-        "/api/activity/attempt/show?attempt_id=" +
-          res[no].attempt_id +
-          "&activity_id=" +
-          query.activity_id
-      );
-      console.log(res2);
-      setActivity(res);
-      setAttempt(res2);
-    } catch {
-      alert(console.error());
-      setAttempt([]);
-      setActivity([]);
-    }
-    setLoading(false);
+    let res, res2;
+    fetchData({
+      before: () => setLoading(true),
+      send: async () => {
+        try {
+          res = await Api.get(
+            "/api/activity/attempts?activity_id=" +
+              query.activity_id +
+              "&student_id=" +
+              student.id
+          );
+          res2 = await Api.get(
+            "/api/activity/attempt/show?attempt_id=" +
+              res[no].attempt_id +
+              "&activity_id=" +
+              query.activity_id
+          );
+        } catch {
+          alert(console.error());
+          setErrors(true);
+          setAttempt([]);
+          setActivity([]);
+        }
+      },
+      after: (data) => {
+        setActivity(res);
+        setAttempt(res2);
+        setLoading(false);
+      },
+    });
   };
   useEffect(() => {
     getActivityDetails();
   }, [query?.activity_id, no]);
   return (
     <React.Fragment>
-      {!loading && (
-        <Box>
-          <Paper
-            style={{
-              padding: "10px",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Box
-              width="100%"
-              display="flex"
-              justifyContent="space-between"
-              flexWrap="wrap"
-            >
-              <IconButton onClick={props.onClose}>
-                <Icon color="primary" fontSize="small">
-                  arrow_back
-                </Icon>
-              </IconButton>
-              <FormControl
-                variant="outlined"
-                className={
-                  "themed-input " +
-                  (theme.palette.type === "dark" ? "light" : "dark")
-                }
-                style={
-                  isMobile
-                    ? {
-                        paddingRight: theme.spacing(2),
-                        marginTop: 32,
-                        width: "100%",
-                      }
-                    : {
-                        marginTop: 32,
-                        width: "10%",
-                      }
-                }
-              >
-                <InputLabel>Attempt</InputLabel>
-                <Select
-                  label="Attempt"
-                  color="primary"
-                  defaultValue={no}
-                  onChange={(e) => {
-                    let val = e.target.value;
-                    setNo(val);
-                  }}
-                >
-                  {activity.map((key, i) => (
-                    <MenuItem value={i} key={i}>
-                      <Typography>{(i = i + 1)}</Typography>
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Box>
-            <Box width="100%" style={{ marginTop: "10px" }}>
-              <Divider />
-            </Box>
-            <Box width={isMobile ? "100%" : "auto"}>
-              <Typography variant="h6">{attempt.title}</Typography>
-            </Box>
-            <Box p={2}>
-              <Typography color="textSecondary" style={{ fontWeight: "bold" }}>
-                Duration: {attempt.duration}
-              </Typography>
-              <Typography color="textSecondary" style={{ fontWeight: "bold" }}>
-                Instructions:
-              </Typography>
-              <Typography
-                color="textSecondary"
-                style={{ fontWeight: "normal" }}
-              >
-                {attempt.instruction}
-              </Typography>
-            </Box>
-          </Paper>
+      {loading && (
+        <Box
+          fullWidth
+          height="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <CircularProgress />
+        </Box>
+      )}
+      {
+        (!loading,
+        !errors && (
           <Box>
-            {attempt?.questionnaires &&
-              attempt.questionnaires.map((data) => {
-                return (
-                  <Typography key={data.id}>
-                    {" "}
-                    {data?.questions &&
-                      data.questions.map((data, i) => {
-                        return (
-                          <Paper
-                            elevation={3}
-                            style={{
-                              marginTop: 30,
-                              padding: 20,
-                              // borderTopLeftRadius: "5.5%",
-                              // borderBottomLeftRadius: "5.5%",
-                              borderLeft: "20px solid",
-                              borderLeftColor: "#5DD583",
-                            }}
-                          >
-                            <Typography key={data.id}>
-                              {i + 1}. {data.question}
-                              <br />
-                              {data.media_url ? (
-                                <Box textAlign="center">
-                                  <img
-                                    src={data.media_url}
-                                    style={{
-                                      maxHeight: isMobile ? 200 : 400,
-                                      maxWidth: isMobile ? 200 : 400,
-                                    }}
-                                  />
-                                </Box>
-                              ) : (
-                                ""
-                              )}
-                              <br />
-                              {data.choices.map((data, i) => {
-                                return (
-                                  <ul>
-                                    {String.fromCharCode(65 + i)}. {data.option}
-                                  </ul>
-                                );
-                              })}
-                              <Typography
-                                color="textSecondary"
-                                style={{ fontWeight: "bold" }}
-                              >
-                                Correct Answer:
+            <Paper
+              style={{
+                padding: "10px",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Box
+                width="100%"
+                display="flex"
+                justifyContent="space-between"
+                flexWrap="wrap"
+              >
+                <IconButton onClick={props.onClose}>
+                  <Icon color="primary" fontSize="small">
+                    arrow_back
+                  </Icon>
+                </IconButton>
+                <FormControl
+                  variant="outlined"
+                  className={
+                    "themed-input " +
+                    (theme.palette.type === "dark" ? "light" : "dark")
+                  }
+                  style={
+                    isMobile
+                      ? {
+                          paddingRight: theme.spacing(2),
+                          marginTop: 32,
+                          width: "100%",
+                        }
+                      : {
+                          marginTop: 32,
+                          width: "10%",
+                        }
+                  }
+                >
+                  <InputLabel>Attempt</InputLabel>
+                  <Select
+                    label="Attempt"
+                    color="primary"
+                    defaultValue={no}
+                    onChange={(e) => {
+                      let val = e.target.value;
+                      setNo(val);
+                    }}
+                  >
+                    {activity.map((key, i) => (
+                      <MenuItem value={i} key={i}>
+                        <Typography>{(i = i + 1)}</Typography>
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Box>
+              <Box width="100%" style={{ marginTop: "10px" }}>
+                <Divider />
+              </Box>
+              <Box width={isMobile ? "100%" : "auto"}>
+                <Typography variant="h6">{attempt.title}</Typography>
+              </Box>
+              <Box p={2}>
+                <Typography
+                  color="textSecondary"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Duration: {attempt.duration}
+                </Typography>
+                <Typography
+                  color="textSecondary"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Instructions:
+                </Typography>
+                <Typography
+                  color="textSecondary"
+                  style={{ fontWeight: "normal" }}
+                >
+                  {attempt.instruction}
+                </Typography>
+              </Box>
+            </Paper>
+            <Box>
+              {attempt?.questionnaires &&
+                attempt.questionnaires.map((data) => {
+                  return (
+                    <Typography key={data.id}>
+                      {" "}
+                      {data?.questions &&
+                        data.questions.map((data, i) => {
+                          return (
+                            <Paper
+                              elevation={3}
+                              style={{
+                                marginTop: 30,
+                                padding: 20,
+                                // borderTopLeftRadius: "5.5%",
+                                // borderBottomLeftRadius: "5.5%",
+                                borderLeft: "20px solid",
+                                borderLeftColor: "#5DD583",
+                              }}
+                            >
+                              <Typography key={data.id}>
+                                {i + 1}. {data.question}
+                                <br />
+                                {data.media_url ? (
+                                  <Box textAlign="center">
+                                    <img
+                                      src={data.media_url}
+                                      style={{
+                                        maxHeight: isMobile ? 200 : 400,
+                                        maxWidth: isMobile ? 200 : 400,
+                                      }}
+                                    />
+                                  </Box>
+                                ) : (
+                                  ""
+                                )}
+                                <br />
                                 {data.choices.map((data, i) => {
                                   return (
-                                    <div>
-                                      {data.is_correct === 1 && (
-                                        <ul
-                                          style={{
-                                            color: "green",
-                                            fontWeight: "bold",
-                                          }}
-                                        >
-                                          {data.option}
-                                        </ul>
-                                      )}
-                                    </div>
+                                    <ul>
+                                      {String.fromCharCode(65 + i)}.{" "}
+                                      {data.option}
+                                    </ul>
                                   );
                                 })}
-                              </Typography>
-                              {data.student_answer && (
                                 <Typography
                                   color="textSecondary"
-                                  style={{
-                                    //  color:
-                                    //    data.student_answer.is_correct === 1
-                                    //      ? setBorder(["green"])
-                                    //     : setBorder("red"),
-                                    fontWeight: "bold",
-                                  }}
+                                  style={{ fontWeight: "bold" }}
                                 >
-                                  Student's Answer:
-                                  <ul
+                                  Correct Answer:
+                                  {data.choices.map((data, i) => {
+                                    return (
+                                      <div>
+                                        {data.is_correct === 1 && (
+                                          <ul
+                                            style={{
+                                              color: "green",
+                                              fontWeight: "bold",
+                                            }}
+                                          >
+                                            {data.option}
+                                          </ul>
+                                        )}
+                                      </div>
+                                    );
+                                  })}
+                                </Typography>
+                                {data.student_answer && (
+                                  <Typography
+                                    color="textSecondary"
                                     style={{
-                                      color:
-                                        data.student_answer.is_correct === 1
-                                          ? "green"
-                                          : "red",
+                                      //  color:
+                                      //    data.student_answer.is_correct === 1
+                                      //      ? setBorder(["green"])
+                                      //     : setBorder("red"),
                                       fontWeight: "bold",
                                     }}
                                   >
-                                    {data.student_answer.answer}
-                                  </ul>
-                                </Typography>
-                              )}
-                            </Typography>
-                          </Paper>
-                        );
-                      })}
-                  </Typography>
-                );
-              })}
+                                    Student's Answer:
+                                    <ul
+                                      style={{
+                                        color:
+                                          data.student_answer.is_correct === 1
+                                            ? "green"
+                                            : "red",
+                                        fontWeight: "bold",
+                                      }}
+                                    >
+                                      {data.student_answer.answer}
+                                    </ul>
+                                  </Typography>
+                                )}
+                              </Typography>
+                            </Paper>
+                          );
+                        })}
+                    </Typography>
+                  );
+                })}
+            </Box>
           </Box>
-        </Box>
-      )}
+        ))
+      }
     </React.Fragment>
   );
 }
