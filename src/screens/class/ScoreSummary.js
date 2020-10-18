a3c4a334b63db16fe58ddb133bab13f68405d358
noobest <52671389+noobest@users.noreply.github.com>
2020-10-17 22:58:09 -0800

    answer details student view initial commit

--- src/screens/class/ScoreSummary.js
+++ src/screens/class/ScoreSummary.js
@@ -17,6 +17,9 @@ import {
   MenuItem,
   Paper,
   useMediaQuery,
+  FormControl,
+  Select,
+  Divider
 } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
@@ -33,6 +36,7 @@ import { SearchInput } from "../../components/Selectors";
 import moment from "moment";
 import Chart from "react-apexcharts";
 import { DialogTitle } from "../../components/dialogs";
+import { makeLinkTo } from "../../components/router-dom";
 const qs = require("query-string");
 
 const activityTypes = [
@@ -46,7 +50,7 @@ const ScoreSummary = (props) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const query = qs.parse(window.location.search);
-  const { class_id } = props.match.params;
+  const { class_id, } = props.match.params;
   const [currentActivity, setCurrentActivity] = useState(query.activity);
   const [scores, setScores] = useState();
   const { userInfo, parentData } = props;
@@ -286,8 +290,10 @@ const ScoreSummary = (props) => {
   );
 };
 function DetailedScores(props) {
+  const query = require("query-string").parse(window.location.search);
+
   const { userInfo, parentData } = props;
-  const { class_id } = props.match.params;
+  const { class_id, schedule_id, option_name, room_name } = props.match.params;
   const [saving, setSaving] = useState();
   const [savingId, setSavingId] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -316,7 +322,24 @@ function DetailedScores(props) {
   useEffect(() => {
     getDetailedScores();
   }, []);
-  return (
+  return query.q && query.activity_id ? (
+    <ActivityDetails
+      onClose={() => {
+        props.history.push(
+          makeLinkTo([
+            "class",
+            class_id,
+            schedule_id,
+            option_name,
+            room_name || "",
+            "?q=" + props.userInfo.id,
+          ])
+        );
+      }}
+      {...props}
+    
+    />
+  ) : (
     <Box>
       <Box
         justifyContent="space-between"
@@ -345,6 +368,7 @@ function DetailedScores(props) {
       {!loading && (
         <MTable
           headers={[
+            { id: "id", title: "ID", width: "33%" },
             { id: "title", title: "Title", width: "33%" },
             {
               id: "published_at",
@@ -415,6 +439,16 @@ function DetailedScores(props) {
               style={{ padding: "30px 0" }}
             >
               <Box width="100%" marginBottom={1}>
+              <Typography
+                  style={{
+                    fontWeight: "bold",
+                    color: "#38108d",
+                    fontSize: "1em",
+                  }}
+                >
+                  ID
+                </Typography>
+                <Typography variant="body1">{item.id}</Typography>
                 <Typography
                   style={{
                     fontWeight: "bold",
@@ -476,8 +510,21 @@ function DetailedScores(props) {
               </Box>
             </Box>
           )}
-          rowRender={(item, { disabled = false }) => (
-            <React.Fragment>
+          rowRender={(item, { disabled = true }) => (
+            <Box style={{display:"flex", flexDirection:"row"}} onClick={(e) => {
+              props.history.push(
+                makeLinkTo([
+                  "class",
+                  class_id,
+                  schedule_id,
+                  option_name,
+                  room_name || "",
+                  "?q=" + props.userInfo.id,
+                  "&activity_id="+item.id,
+                ])
+              );}}>
+              
+              <Box width="33%">{item.id}</Box>
               <Box width="33%">{item.title}</Box>
               <Box width="33%" textAlign="center">
                 {moment(item.published_at).format("MMM DD, YYYY")}
@@ -497,13 +544,215 @@ function DetailedScores(props) {
                   precision={0.1}
                 />
               </Box>
-            </React.Fragment>
+              
+            </Box >
           )}
         />
       )}
     </Box>
   );
 }
+
+function ActivityDetails(props) {
+  const query = require("query-string").parse(window.location.search);
+  const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+  const [no, setNo] = useState(0);
+  const [activity, setActivity] = useState([]);
+  const [attempt, setAttempt] = useState([]);
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const getActivityDetails = async () => {
+   
+    setLoading(true);
+    try {
+      let res = await Api.get(
+        "/api/activity/attempts?activity_id=" +
+          query.activity_id +
+          "&student_id=" +
+          props.userInfo.id
+      );
+      let res2 = await Api.get(
+        "/api/activity/attempt/show?attempt_id=" +
+          res[no].attempt_id +
+          "&activity_id=" +
+          query.activity_id
+      );
+      setActivity(res);
+      setAttempt(res2);
+    } catch {
+      alert(console.error());
+      setAttempt([]);
+      setActivity([]);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getActivityDetails();
+  }, [query?.activity_id, no]);
+  return (
+    <React.Fragment>
+      {!loading && (
+        <Box>
+          <Paper
+            style={{
+              padding: "10px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Box
+              width="100%"
+              display="flex"
+              justifyContent="space-between"
+              flexWrap="wrap"
+            >
+              <IconButton onClick={props.onClose}>
+                <Icon color="primary" fontSize="small">
+                  arrow_back
+                </Icon>
+              </IconButton>
+              <FormControl
+                variant="outlined"
+                className={
+                  "themed-input " +
+                  (theme.palette.type === "dark" ? "light" : "dark")
+                }
+                style={
+                  isMobile
+                    ? {
+                        paddingRight: theme.spacing(2),
+                        marginTop: 32,
+                        width: "100%",
+                      }
+                    : {
+                        width: "10%",
+                      }
+                }
+              >
+                <Typography style={{ textAlign: "center", fontWeight: "bold" }}>
+                  Attempt
+                </Typography>
+                <Select
+                  label="Attempt"
+                  color="primary"
+                  onChange={(e) => {
+                    let val = e.target.value;
+                    setNo(val);
+                  }}
+                >
+                  {activity.map((key, i) => (
+                    <MenuItem value={i} key={i}>
+                      <Typography>{(i = i + 1)}</Typography>
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+            <Box width="100%" style={{ marginTop: "10px" }}>
+              <Divider />
+            </Box>
+            <Box width={isMobile ? "100%" : "auto"}>
+              <Typography variant="h6">{attempt.title}</Typography>
+            </Box>
+            <Box p={2}>
+              <Typography color="textSecondary" style={{ fontWeight: "bold" }}>
+                Duration: {attempt.duration}
+              </Typography>
+              <Typography color="textSecondary" style={{ fontWeight: "bold" }}>
+                Instructions:
+              </Typography>
+              <Typography
+                color="textSecondary"
+                style={{ fontWeight: "normal" }}
+              >
+                {attempt.instruction}
+              </Typography>
+            </Box>
+          </Paper>
+          <Box>
+            <Paper style={{ padding: 20, marginTop: 30 }}>
+              {" "}
+              {attempt?.questionnaires &&
+                attempt.questionnaires.map((data) => {
+                  return (
+                    <Typography key={data.id}>
+                      {" "}
+                      {data?.questions &&
+                        data.questions.map((data, i) => {
+                          return (
+                            <Typography key={data.id}>
+                              {i + 1}. {data.question}
+                              <br />
+                              {data.media_url ? (
+                                <Box textAlign="center">
+                                  <img
+                                    src={data.media_url}
+                                    style={{
+                                      maxHeight: isMobile ? 200 : 400,
+                                      maxWidth: isMobile ? 200 : 400,
+                                    }}
+                                  />
+                                </Box>
+                              ) : (
+                                ""
+                              )}
+                              <br />
+                              {data.choices.map((data, i) => {
+                                return (
+                                  <ul>
+                                    {String.fromCharCode(65 + i)}. {data.option}
+                                  </ul>
+                                );
+                              })}
+                              <Typography
+                                color="textSecondary"
+                                style={{ fontWeight: "bold" }}
+                              >
+                                Correct Answer:
+                                {data.choices.map((data, i) => {
+                                  return (
+                                    <div>
+                                      {data.is_correct === 1 && (
+                                        <ul
+                                          style={{
+                                            color: "green",
+                                            fontWeight: "bold",
+                                          }}
+                                        >
+                                          {data.option}
+                                        </ul>
+                                      )}
+                                    </div>
+                                  );
+                                })}
+                              </Typography>
+                              {data.student_answer && (
+                                <Typography
+                                  color="textSecondary"
+                                  style={{ fontWeight: "bold" }}
+                                >
+                                  Student's Answer: <ul  style={{
+                                            color:data.student_answer.is_correct === 1? "green":"red",
+                                            fontWeight: "bold"}}>{data.student_answer.answer}</ul>
+                                </Typography>
+                              )}
+                              <hr />
+                            </Typography>
+                          );
+                        })}
+                    </Typography>
+                  );
+                })}
+            </Paper>
+          </Box>
+        </Box>
+      )}
+    </React.Fragment>
+  );
+}
+
 const useStyles = makeStyles((theme) => ({
   parentWrapper: {
     width: "100%",
